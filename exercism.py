30fda24b74a3412d2bd048875e31d6d8a0ca83ee
mohammad <m.raeisi71@example.com>
2019-01-20 13:51:38 +0330

    exercism2&3.py

--- exercism.py
+++ exercism.py
@@ -1,5 +1,16 @@
 
 
-#Say "Hello, World!" With Python
+#1-Say "Hello, World!" With Python
 
-print("Hello, World!")
+#print("Hello, World!")
+#--------------------------------------------------------------------------
+#2-Create a sentence of the form "One for X, one for me."
+
+def name1(name="you"):
+    return f'One for {name}, one for me.'
+print(name1("ali"))
+
+#--------------------------------------------------------------------------
+#3-Convert a long phrase to its acronym
+x=input("Input Your Long Phrase:")
+print(x)
