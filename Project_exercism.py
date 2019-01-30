a996c723f9d6222bf6a2212e4e50de1a19d40a7d
mohammad <m.raeisi71@example.com>
2019-01-30 05:33:17 +0330

    Project52.py

--- Project_exercism.py
+++ Project_exercism.py
@@ -171,3 +171,9 @@ def slices(series, length):
 
 print(slices("56896", 6))
 ---------------------------------------------
+def sum_of_multiples(limit, multiples):
+    return sum(set([m for n in multiples for m in getMultiples(n, limit)]))
+
+def getMultiples(n, limit):
+    if n == 0: return []
+    return [n * factor for factor in range(1, limit) if n * factor < limit]
